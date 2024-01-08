5bbde385dca85146a371d3b8f9625ac1e078c09a
Lokendra Bairwa <lokendra.bairwa@front.page>
2024-01-08 08:12:57 +0530

    feat(rate-limit): Add rate limiter middleware to limit incoming requests per IP

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import express, {Express, Request, Response, NextFunction} from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+import rateLimit from 'express-rate-limit';
+
 dotenv.config();
 
 import L from './utils/logger';
@@ -22,6 +24,13 @@ app.listen(PORT, () => {
     L.info(`Server is running on port ${PORT}`);
 });
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+});
+
+app.use(limiter);
 app.use('/api/v1/posts', postV1Router);
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     res.status(err.status || 500).send({
